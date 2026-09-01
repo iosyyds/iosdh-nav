a4b10899b8e215fc11d30d7b1b399d308b190d55
甜甜 <nbios@qq.com>
2026-09-01 16:57:19 +0800

    feat: add category data module part2 (9 categories)

---
+++ lib/categories_part2.ts
@@ -0,0 +1,90 @@
+// 甜甜导航 - 站点数据（自动生成）
+export interface Site {
+  id: string;
+  name: string;
+  url: string;
+  desc: string;
+  icon?: string;
+}
+export const categories_part2: Record<string, Site[]> = {
+  "交互动效": [
+    { id: "162", name: "Adobe After Effects CC", url: "https://www.adobe.com/cn/products/aftereffects", desc: "电影般的视觉效果和动态图形。" },
+    { id: "163", name: "principle", url: "http://principleformac.com", desc: "Animate Your Ideas, Design Better Apps" },
+    { id: "164", name: "flinto", url: "https://www.flinto.com", desc: "Flinto is a Mac app used by top designers around the world to create interactive and animated prototypes of their app designs." },
+    { id: "166", name: "ProtoPie", url: "http://www.protopie.cn", desc: "高保真交互原型设计" },
+    { id: "165", name: "framer", url: "https://framer.com", desc: "Design everything from detailed icons to high-fidelity interactions—all in one place." },
+  ],
+  "图形创意": [
+    { id: "150", name: "Affinity Designer", url: "https://affinity.serif.com", desc: "专业创意软件" },
+    { id: "149", name: "photoshop", url: "https://www.adobe.com/cn/products/photoshop.html", desc: "Photoshop不需要解释" },
+    { id: "155", name: "Blender", url: "https://www.blender.org", desc: "Blender is the free and open source 3D creation suite." },
+    { id: "151", name: "Illustrator", url: "https://www.adobe.com/cn/products/illustrator", desc: "矢量图形和插图。" },
+    { id: "152", name: "indesign", url: "http://www.adobe.com/cn/products/indesign.html", desc: "页面设计、布局和出版。" },
+    { id: "154", name: "3ds-max", url: "https://www.autodesk.com/products/3ds-max/overview", desc: "3D modeling, animation, and rendering software" },
+    { id: "153", name: "cinema-4d", url: "https://www.maxon.net/en/products/cinema-4d/overview", desc: "Cinema 4D is the perfect package for all 3D artists who want to achieve breathtaking results fast and hassle-free." },
+  ],
+  "在线工具": [
+    { id: "188", name: "稿定抠图", url: "https://www.gaoding.com", desc: "免费在线抠图软件,图片快速换背景-抠白底图" },
+    { id: "183", name: "goqr", url: "http://goqr.me", desc: "create QR codes for free (Logo, T-Shirt, vCard, EPS)" },
+    { id: "184", name: "ezgif", url: "https://ezgif.com", desc: "simple online GIF maker and toolset for basic animated GIF editing." },
+    { id: "186", name: "screen sizes", url: "http://screensiz.es", desc: "Viewport Sizes and Pixel Densities for Popular Devices" },
+    { id: "185", name: "Android 9 patch", url: "http://inloop.github.io/shadow4android", desc: "Android 9-patch shadow generator fully customizable shadows" },
+    { id: "187", name: "svgomg", url: "https://jakearchibald.github.io/svgomg", desc: "SVG在线压缩平台" },
+    { id: "182", name: "tinypng", url: "https://tinypng.com", desc: "Optimize your images with a perfect balance in quality and file size." },
+  ],
+  "界面设计": [
+    { id: "161", name: "figma", url: "https://www.figma.com", desc: "Design, prototype, and gather feedback all in one place with Figma." },
+    { id: "160", name: "Muse CC", url: "https://creative.adobe.com/zh-cn/products/download/muse", desc: "无需利用编码即可进行网站设计。" },
+    { id: "158", name: "invisionapp", url: "https://www.invisionapp.com", desc: "Powerful design prototyping tools" },
+    { id: "159", name: "marvelapp", url: "https://marvelapp.com", desc: "Simple design, prototyping and collaboration" },
+    { id: "157", name: "Adobe XD", url: "http://www.adobe.com/products/xd.html", desc: "Introducing Adobe XD. Design. Prototype. Experience." },
+    { id: "156", name: "Sketch", url: "https://sketchapp.com", desc: "The digital design toolkit" },
+  ],
+  "LOGO设计": [
+    { id: "61", name: "World Vector Logo", url: "https://worldvectorlogo.com", desc: "Brand logos free to download" },
+    { id: "62", name: "Instant Logo Search", url: "http://instantlogosearch.com", desc: "Search & download thousands of logos instantly" },
+    { id: "60", name: "iOS Icon Gallery", url: "http://iosicongallery.com", desc: "Showcasing beautiful icon designs from the iOS App Store" },
+    { id: "58", name: "Iconsfeed", url: "http://www.iconsfeed.com", desc: "Looking for iOS icon inspiration? We bring the iOS icons gallery to you." },
+  ],
+  "Mockup": [
+    { id: "113", name: "Threed", url: "http://threed.io", desc: "Generate 3D Mockups right in your Browser" },
+    { id: "111", name: "Dunnnk", url: "http://dunnnk.com", desc: "Generate Product Mockups For Free" },
+    { id: "110", name: "MockupZone", url: "https://mockup.zone", desc: "Mockup Zone is an online store where you can find free and premium PSD mockup files to show your designs in a professional way." },
+    { id: "114", name: "Mockup World", url: "https://free.lstore.graphics", desc: "The best free Mockups from the Web" },
+    { id: "112", name: "Graphberry", url: "http://www.graphberry.com", desc: "Free design resources, Mockups, PSD web templates, Icons" },
+    { id: "117", name: "For Graphic TM", url: "http://forgraphictm.com", desc: "High Quality PSD Mockups for Graphic Designers." },
+    { id: "116", name: "pixeden", url: "https://www.pixeden.com", desc: "free web resources and graphic design templates." },
+    { id: "115", name: "Lstore", url: "https://free.lstore.graphics", desc: "Exclusive mindblowing freebies for designers and developers" },
+  ],
+  "PPT资源": [
+    { id: "143", name: "OfficePLUS", url: "http://www.officeplus.cn/Template/Home.shtml", desc: "OfficePLUS，微软Office官方在线模板网站！" },
+    { id: "148", name: "5百丁", url: "http://ppt.500d.me", desc: "中国领先的PPT模板共享平台" },
+    { id: "144", name: "优品PPT", url: "http://www.ypppt.com", desc: "高质量的模版，而且还有PPT图表，PPT背景图等资源" },
+    { id: "145", name: "PPT+", url: "http://www.pptplus.cn", desc: "PPT加直播、录制和分享—PPT+语音内容分享平台" },
+    { id: "146", name: "PPTMind", url: "http://www.pptmind.com", desc: "分享高端ppt模板与keynote模板的数字作品交易平台" },
+    { id: "147", name: "tretars", url: "http://www.tretars.com/ppt-templates", desc: "The best free Mockups from the Web" },
+  ],
+  "Sketch资源": [
+    { id: "91", name: "Sketch中文网", url: "http://www.sketchcn.com", desc: "分享最新的Sketch中文手册" },
+    { id: "87", name: "Sketch Measure", url: "http://utom.design/measure", desc: "Friendly user interface offers you a more intuitive way of making marks." },
+    { id: "88", name: "Sketch App Sources", url: "https://www.sketchappsources.com", desc: "Free design resources and plugins - Icons, UI Kits, Wireframes, iOS, Android Templates for Sketch" },
+    { id: "94", name: "Sketchcasts", url: "https://www.sketchcasts.net", desc: "Learn Sketch Train your design skills with a weekly video tutorial" },
+    { id: "90", name: "Sketch Hunt", url: "http://sketchhunt.com", desc: "Sketch Hunt is an independent blog sharing gems in learning, plugins & design tools for fans of Sketch app." },
+    { id: "86", name: "Sketch", url: "https://sketchapp.com", desc: "The digital design toolkit" },
+    { id: "93", name: "Awesome Sketch Plugins", url: "https://awesome-sket.ch", desc: "A collection of really useful Sketch plugins." },
+  ],
+  "图标素材": [
+    { id: "55", name: "Font Awesome Icon", url: "https://fontawesome.com/icons", desc: "The complete set of 675 icons in Font Awesome" },
+    { id: "57", name: "Simpleline Icons", url: "http://simplelineicons.com", desc: "Simple line Icons pack" },
+    { id: "53", name: "UICloud", url: "http://ui-cloud.com", desc: "The largest user interface design database in the world." },
+    { id: "54", name: "Material icons", url: "https://material.io/icons", desc: "Access over 900 material system icons, available in a variety of sizes and densities, and as a web font." },
+    { id: "48", name: "Icon Archive", url: "http://www.iconarchive.com", desc: "Search 590,912 free icons" },
+    { id: "50", name: "IcoMoonApp", url: "https://icomoon.io/app", desc: "Icon Font, SVG, PDF & PNG Generator" },
+    { id: "51", name: "easyicon", url: "http://www.easyicon.net", desc: "PNG、ICO、ICNS格式图标搜索、图标下载服务" },
+    { id: "46", name: "iconfont", url: "http://www.iconfont.cn", desc: "阿里巴巴矢量图标库" },
+    { id: "52", name: "flaticon", url: "https://www.flaticon.com", desc: "634,000+ Free vector icons in SVG, PSD, PNG, EPS format or as ICON FONT." },
+    { id: "45", name: "Iconfinder", url: "https://www.iconfinder.com", desc: "2,100,000+ free and premium vector icons." },
+    { id: "47", name: "iconmonstr", url: "https://iconmonstr.com", desc: "Free simple icons for your next project" },
+    { id: "49", name: "FindIcons", url: "https://findicons.com", desc: "Search through 300,000 free icons" },
+  ],
+};
